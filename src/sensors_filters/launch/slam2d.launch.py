9107460801100c027dfc1873df07723ef912a4ba
Gonçalo <goncalopikin@gmail.com>
2026-05-11 11:40:33 +0100

    estrutura para slam3d adicionada

--- src/sensors_filters/launch/slam2d.launch.py
+++ src/sensors_filters/launch/slam2d.launch.py
@@ -1,10 +1,9 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     # packages paths
